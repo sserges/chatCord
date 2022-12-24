2624e40def9e7f6a874188c8cc821064cb58606a
sserges <agounkeserge@gmail.com>
2022-12-24 17:05:14 -0400

    Readme added

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const express = require('express')
 const socketio = require('socket.io')
 
 const formatMessage = require('./utils/messages')
-const { userJoin, getCurrentUser } = require('./utils/users')
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -32,6 +37,12 @@ io.on('connection', (socket) => {
         'message',
         formatMessage(botName, `${user.username} has joined the chat`)
       )
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    })
   })
 
   // Listen for chatMessage
@@ -43,7 +54,20 @@ io.on('connection', (socket) => {
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat'))
+    const user = userLeave(socket.id)
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      )
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      })
+    }
   })
 })
 
